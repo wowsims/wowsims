8408bd886c9276788582e7975c39f6a69b06c306
Adrian Klingen <adrian.klingen@deptagency.com>
2024-05-13 11:35:07 +0200

    Finalise edits

--- packages/ui/Button/Button.tsx
+++ packages/ui/Button/Button.tsx
@@ -1,31 +1,63 @@
 import clsx from "clsx";
 import { ButtonHTMLAttributes, PropsWithChildren, forwardRef } from "react";
 
+import "./variants/close.scss";
+import "./variants/reset.scss";
+
+export type ButtonVariant =
+  | "primary"
+  | "outline-primary"
+  | "secondary"
+  | "outline-secondary"
+  | "link"
+  | "close"
+  | "reset"
+  | "clear";
+
+export type ButtonSize = "sm" | "lg";
 export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   // If variant is defined we will add the .btn class as well
-  variant?:
-    | "primary"
-    | "outline-primary"
-    | "secondary"
-    | "outline-secondary"
-    | "link"
-    | "close"
-    | "reset"
-    | "clear";
+  variant?: ButtonVariant;
+  size?: ButtonSize;
+  // String of className's for the left icon
+  iconLeft?: string;
+  // String of className's for the right icon
+  iconRight?: string;
 };
 
 export const Button = forwardRef<
   HTMLButtonElement,
   PropsWithChildren<ButtonProps>
->(({ className, children, variant, type = "button", ...props }, ref) => {
-  return (
-    <button
-      ref={ref}
-      className={clsx(variant && `btn btn-${variant}`, className)}
-      type={type}
-      {...props}
-    >
-      {children}
-    </button>
-  );
-});
+>(
+  (
+    {
+      className,
+      children,
+      variant,
+      size,
+      type = "button",
+      iconLeft,
+      iconRight,
+      ...props
+    },
+    ref,
+  ) => {
+    return (
+      <button
+        ref={ref}
+        className={clsx(
+          variant && variant !== "close" && `btn`,
+          variant && `btn-${variant}`,
+          size && `btn-${size}`,
+          className,
+        )}
+        type={type}
+        {...props}
+      >
+        {iconLeft && <i className={iconLeft} />}
+        {children}
+        {iconRight && <i className={iconRight} />}
+      </button>
+    );
+  },
+);
--- packages/ui/Link/Link.tsx
+++ packages/ui/Link/Link.tsx
@@ -5,19 +5,12 @@ export type LinkProps = AnchorHTMLAttributes<HTMLAnchorElement> & {
   variant?: undefined;
 };
 
-export const Link = forwardRef<
-  HTMLAnchorElement,
-  PropsWithChildren<LinkProps>
->(({ className, children, variant, href, role, ...props }, ref) => {
-  return (
-    <a
-      ref={ref}
-      href={href}
-      className={clsx("btn", variant && `btn-${variant}`, className)}
-      role={role}
-      {...props}
-    >
-      {children}
-    </a>
-  );
-});
+export const Link = forwardRef<HTMLAnchorElement, PropsWithChildren<LinkProps>>(
+  ({ className, children, variant, href, role, ...props }, ref) => {
+    return (
+      <a ref={ref} href={href} className={className} role={role} {...props}>
+        {children}
+      </a>
+    );
+  },
+);
